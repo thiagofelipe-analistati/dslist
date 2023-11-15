f0ab24b2e6d2bc69c3eef925f0d9973610069cc1
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2023-11-15 10:08:46 -0300

    Create Projet

---
+++ src/main/java/com/example/demo/Dslist1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslist1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslist1Application.class, args);
+	}
+
+}
